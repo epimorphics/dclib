91413aaca2ffc72ee6f9c89754614ce86bdb0742
der <dave@epimorphics.com>
2013-12-24 16:58:17 +0000

    Simple file store abstraction

---
+++ src/main/java/com/epimorphics/dclib/storage/DirectoryFileStore.java
@@ -0,0 +1,74 @@
+/******************************************************************
+ * File:        DirectoryFileStore.java
+ * Created by:  Dave Reynolds
+ * Created on:  24 Dec 2013
+ * 
+ * (c) Copyright 2013, Epimorphics Limited
+ *
+ *****************************************************************/
+
+package com.epimorphics.dclib.storage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.epimorphics.util.EpiException;
+import com.epimorphics.util.FileUtil;
+
+/**
+ * File store implementation that uses simple local directories.
+ * 
+ * @author <a href="mailto:dave@epimorphics.com">Dave Reynolds</a>
+ */
+public class DirectoryFileStore implements FileStore {
+    protected File root;
+    
+    public void setRoot(String rootDir) {
+        root = new File(rootDir);
+        if ( ! (root.exists() && root.canRead() && root.canWrite()) ) {
+            throw new EpiException("Can't access file store root: " + rootDir);
+        }
+    }
+    
+    @Override
+    public List<String> list(String folder) {
+        String[] files = root.list();
+        List<String> results = new ArrayList<>( files.length );
+        for (String file : files) {
+            results.add(file);
+        }
+        return results;
+    }
+
+    @Override
+    public boolean exists(String path) {
+        return new File(root, path).exists();
+    }
+
+    @Override
+    public InputStream read(String path) throws IOException {
+        return new FileInputStream( new File(root, path) );
+    }
+
+    @Override
+    public void makeFolder(String path) throws IOException {
+        FileUtil.ensureDir( new File(root, path).getPath() );
+    }
+
+    @Override
+    public OutputStream write(String path) throws IOException {
+        return new FileOutputStream(new File(root, path), false);
+    }
+
+    @Override
+    public OutputStream write(String path, boolean append) throws IOException {
+        return new FileOutputStream(new File(root, path), append);
+    }
+
+}
---
+++ src/main/java/com/epimorphics/dclib/storage/FileStore.java
@@ -0,0 +1,59 @@
+/******************************************************************
+ * File:        FileStore.java
+ * Created by:  Dave Reynolds
+ * Created on:  24 Dec 2013
+ * 
+ * (c) Copyright 2013, Epimorphics Limited
+ *
+ *****************************************************************/
+
+package com.epimorphics.dclib.storage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Interface for a storage facility where source data, templates and
+ * so forth can be stored for access. Will want implementations for shared
+ * storage services like S3 as well as simple file system.
+ * <p>
+ * All implementations support a path syntax with "/" as the separator.
+ * </p>
+ * 
+ * @author <a href="mailto:dave@epimorphics.com">Dave Reynolds</a>
+ */
+public interface FileStore {
+
+    /**
+     * List all files/objects in a named folder
+     */
+    public List<String> list(String folder);
+    
+    /**
+     * Return true if a folder or file exists at the given location
+     */
+    public boolean exists(String path);
+    
+    /**
+     * Open a file/object for reading
+     */
+    public InputStream read(String path) throws IOException;
+    
+    /**
+     * Create a new folder 
+     */
+    public void makeFolder(String path) throws IOException;
+    
+    /**
+     * Create a new file/object for writing. Replaces any existing file
+     */
+    public OutputStream write(String path) throws IOException;
+    
+    /**
+     * Create a new file/object for writing
+     * @param append if true then appends to an existing file
+     */
+    public OutputStream write(String path, boolean append) throws IOException;
+}
